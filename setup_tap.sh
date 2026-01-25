768fbd659f0619dc7db90066ba78c3d1f0ac2b9c
danshani <danshani11@gmail.com>
2026-01-25 15:01:59 +0200

    scripts: update TAP configuration to align with the 10.0.0.5 stack IP

--- setup_tap.sh
+++ setup_tap.sh
@@ -8,11 +8,11 @@ echo "Setting up TAP device: $TAP_DEV"
 # 1. יצירת הממשק
 sudo ip tuntap add mode tap $TAP_DEV
 
-# 2. הגדרת ה-IP של המחשב שלך (הצד של ה-Host)
+# 2. הגדרת ה-IP של המחשב שלך (Host) - נשאר 10.0.0.1
 sudo ip addr add 10.0.0.1/24 dev $TAP_DEV
 
 # 3. הפעלת הממשק
 sudo ip link set dev $TAP_DEV up
 
 echo "TAP device is ready. Host IP: 10.0.0.1"
-echo "The TCP Stack will use: 10.0.0.2"
+echo "The TCP Stack will use: 10.0.0.5" # עדכון ההודעה ל-10.0.0.5
